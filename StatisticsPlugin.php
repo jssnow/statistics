c7eea3c81c0dc064d48754b3fd80a2ce3ccbba9a
jhj <jhj767658181@gmail.com>
2017-09-13 16:34:51 +0800

    忘了提交的什么了

--- StatisticsPlugin.php
+++ StatisticsPlugin.php
@@ -64,28 +64,7 @@ class StatisticsPlugin extends Plugin
         if(file_exists($register_png)){
             unlink($register_png);
         }
-//        $mysql = Db::query("select VERSION() as version");
-//        $mysql = $mysql[0]['version'];
-//        $mysql = empty($mysql) ? lang('UNKNOWN') : $mysql;
-//
-//        $version = THINKCMF_VERSION;
-//
-//        //server infomation
-//        $info = [
-//            lang('OPERATING_SYSTEM')      => PHP_OS.'sss',
-//            lang('OPERATING_ENVIRONMENT') => $_SERVER["SERVER_SOFTWARE"],
-//            lang('PHP_VERSION')           => PHP_VERSION,
-//            lang('PHP_RUN_MODE')          => php_sapi_name(),
-//            lang('PHP_VERSION')           => phpversion(),
-//            lang('MYSQL_VERSION')         => $mysql,
-//            'ThinkPHP'                    => THINK_VERSION,
-//            'ThinkCMF'                    => "{$version} <a href=\"http://www.thinkcmf.com\" target=\"_blank\">访问官网</a>",
-//            lang('UPLOAD_MAX_FILESIZE')   => ini_get('upload_max_filesize'),
-//            lang('MAX_EXECUTION_TIME')    => ini_get('max_execution_time') . "s",
-//            //TODO 增加更多信息
-//            lang('DISK_FREE_SPACE')       => round((@disk_free_space(".") / (1024 * 1024)), 2) . 'M',
-//            lang('REG_NUM')               => 243,
-//        ];
+
         //如果移动了入口文件的位置,则这里文件路径需要修改
         require_once "./plugins/statistics/jpgraph/jpgraph.php";
         require_once "./plugins/statistics/jpgraph/jpgraph_line.php";
@@ -93,53 +72,109 @@ class StatisticsPlugin extends Plugin
 
         //从数据库中获取一个月内每天的新注册用户的数量
         //处理时间戳获取当月月份作为筛选条件
-        $month = date('Ym',time());
+        $month = date('Y-m',time());
         $prefix = config('database.prefix');
-        $res = Db::query("select FROM_UNIXTIME(create_time,'%Y%m%d') days,count(id) count from ".$prefix."user WHERE FROM_UNIXTIME(create_time,'%Y%m') = '".$month."' group by days");
-        dump($res);
-//        exit;
-        if(!empty($info)){
-
-        }else{
-            //TODO 最近七天没有数据
-        }
+        $res = Db::query("select FROM_UNIXTIME(create_time,'%Y-%m-%d') days,count(id) count from ".$prefix."user WHERE FROM_UNIXTIME(create_time,'%Y-%m') = '".$month."' group by days");
         // x 轴数据，作为 x 轴标注
         $j = date("t"); //获取当前月份天数
         $start_time = strtotime(date('Y-m-01'));  //获取本月第一天时间戳
         $xdata = array();
-        for($i=0;$i<$j;$i++){
+        for($i=0;$i<$j;$i++)
+        {
             $xdata[] = date('Y-m-d',$start_time+$i*86400); //每隔一天赋值给数组
         }
         //处理获取到的数据
         $ydata = array();
 
-        for ($i=0;$i<33;$i++){
-            foreach ($res as $v){
-                if(intval(substr($v['days'],-1,2)) == $i){
-                    $ydata[$i] = $v['count'];
-                }else{
-                    $ydata[$i] = 0;
+        if(!empty($res))
+        {
+            foreach ($xdata as $k=>&$v)
+            {
+                foreach ($res as $kk=>$vv)
+                {
+                    if($v == $vv['days'])
+                    {
+                        $ydata[$k] = $vv['count'];
+                        break;
+                    }else{
+                        $ydata[$k] = 0;
+                        continue;
+                    }
                 }
+                $v = substr($v,-2);
+            }
+        }else{
+            foreach ($xdata as $k=>$v)
+            {
+                $ydata[$k] = 0;
             }
         }
-        dump($ydata);
-        exit();
+
 
         //将要用于图表创建的数据存放在数组中
-        $data = array(19,23,34,38,45,67,711,78,825,837,90,966);
-        $graph = new \Graph(400,300); //创建新的Graph对象
-        $graph->SetScale("textlin"); //设置刻度样式
-        $graph->img->SetMargin(30,30,80,30); //设置图表边界
-        $graph->title->Set("new users "); //设置图表标题
+//        $data = array(19,23,34,38,45,67,711,78,825,837,90,966);
+//        $graph = new \Graph(1000,300); //创建新的Graph对象
+//        $graph->SetScale("textlin"); //设置刻度样式
+//        $graph->img->SetMargin(30,30,80,30); //设置图表边界
+//        $graph->title->Set("new users count (" . date("m") . "Month)"); //设置图表标题
+//
+//        // Create the linear plot
+//        $lineplot=new \LinePlot($ydata); // 创建新的LinePlot对象
+//        $lineplot->SetLegend("Change"); //设置图例文字
+//        $lineplot->SetColor("red"); // 设置曲线的颜色
+//
+//        // Add the plot to the graph
+//        $graph->Add($lineplot); //在统计图上绘制曲线
+//        // 加入 x 轴标注
+//        $graph->xaxis->SetTickLabels($xdata);
+//        // y 轴坐标描点形状为菱形
+//        $lineplot-> mark->SetType(MARK_DIAMOND );
+//        // 设置 x 轴标注文字为斜体，粗体，6号小字
+//        $graph->xaxis->SetFont(FF_ARIAL,FS_BOLD,10);
+//        // 阴影效果
+//        $graph->SetShadow();
+//        // Display the graph
+//        $graph->Stroke($register_png); //输出图像
+        // Create the graph. These two calls are always required
+        $graph = new \Graph(800,350,'auto');
+        $graph->SetScale("textlin");
 
-        // Create the linear plot
-        $lineplot=new \LinePlot($data); // 创建新的LinePlot对象
-        $lineplot->SetLegend("Amount(M dollars)"); //设置图例文字
-        $lineplot->SetColor("red"); // 设置曲线的颜色
-        // Add the plot to the graph
-        $graph->Add($lineplot); //在统计图上绘制曲线
-        // Display the graph
-        $graph->Stroke($register_png); //输出图像
+        $theme_class=new \UniversalTheme;
+        $graph->SetTheme($theme_class);
+
+//        $graph->yaxis->SetTickPositions(array(0,30,60,90,120,150), array(15,45,75,105,135));
+        $graph->SetBox(false);
+
+        $graph->ygrid->SetFill(false);
+        $graph->xaxis->SetTickLabels($xdata);
+        $graph->yaxis->HideLine(false);
+        $graph->yaxis->HideTicks(false,false);
+
+// Create the bar plots
+        $b1plot = new \BarPlot($ydata);
+//        $b2plot = new BarPlot($data2y);
+//        $b3plot = new BarPlot($data3y);
+
+// Create the grouped bar plot
+        $gbplot = new \GroupBarPlot(array($b1plot));
+// ...and add it to the graPH
+        $graph->Add($gbplot);
+
+
+        $b1plot->SetColor("white");
+        $b1plot->SetFillColor("#cc1111");
+
+//        $b2plot->SetColor("white");
+//        $b2plot->SetFillColor("#11cccc");
+//
+//        $b3plot->SetColor("white");
+//        $b3plot->SetFillColor("#1111cc");
+
+        $graph->title->Set("Number of new registered users (".$month.")");
+
+// Display the graph
+        $graph->Stroke($register_png);
+
         $this->assign('info', $register_png);
         return [
             'width'  => 12,
